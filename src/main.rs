5b82226788da0a2e1576045690d794817df7e7f7
George Madrid <gmadrid@gmail.com>
2018-11-15 08:09:13 -0500

    Moved some code from main into helpers.

--- src/main.rs
+++ src/main.rs
@@ -2,38 +2,49 @@ extern crate itools;
 
 use std::error::Error;
 use std::fs::File;
+use std::path::{Path, PathBuf};
 
 use itools::neardups::{
     bool_to_option, expand_file_list, new_counter, output::Output, Config, Hasher, ItoolsError,
     PersistedCache, Result, SpinnerReader,
 };
 
+fn load_or_create_cache_file<T>(cache_file: T) -> Result<PersistedCache>
+where
+    T: AsRef<Path>,
+{
+    if !cache_file.as_ref().exists() {
+        Ok(PersistedCache::new())
+    } else {
+        let file = File::open(cache_file)?;
+        let r = SpinnerReader::new(file, "Loading cache file...");
+        PersistedCache::from_reader(r)
+    }
+}
+
+fn filter_files_in_cache(files: &Vec<PathBuf>, cache: &PersistedCache) -> Vec<PathBuf> {
+    files
+        .iter()
+        .flat_map(|f| {
+            if cache.contains_file(f) {
+                None
+            } else {
+                Some(f.clone())
+            }
+        }).collect::<Vec<PathBuf>>()
+}
+
 fn run() -> Result<()> {
     let config = Config::new()?;
 
     // If we fail to load the cached file, report an error to avoid overwriting data.
     // If the file doesn't exist, then go ahead and create a brand new one.
-    let mut cache = if !config.cache_file.exists() {
-        PersistedCache::new()
-    } else {
-        let file = File::open(&config.cache_file)?;
-        let r = SpinnerReader::new(file, "Loading cache file...");
-        let val = match PersistedCache::from_reader(r) {
-            Ok(c) => c,
-            Err(e) => return Err(e),
-        };
-        val
-    };
+    let mut cache = load_or_create_cache_file(&config.cache_file)?;
 
     // TODO: report the missing files.
     let (files, _missing) = expand_file_list(config.files)?;
 
-    let mut files_to_hash = Vec::new();
-    for filename in &files {
-        if !cache.contains_file(filename) {
-            files_to_hash.push(filename.to_owned());
-        }
-    }
+    let files_to_hash = filter_files_in_cache(&files, &cache);
 
     let num_files = files_to_hash.len() as u64;
     let (hasher, agg_rx) = Hasher::run(files_to_hash);
